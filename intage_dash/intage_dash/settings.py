04b33915c35944de1d24971e67b9d4866f303a84
zkan <kan@prontomarketing.com>
2017-06-13 14:04:50 +0700

    Change to use postgres database

--- intage_dash/intage_dash/settings.py
+++ intage_dash/intage_dash/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'intage_dash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'PORT': '5432',
     }
 }
 
