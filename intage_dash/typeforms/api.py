65c8aa1b88d9983d4d3891b3d1062dc87aaf2bdd
zkan <kan@prontomarketing.com>
2017-06-14 09:49:47 +0700

    Get json from get_form_data method

--- intage_dash/typeforms/api.py
+++ intage_dash/typeforms/api.py
@@ -6,7 +6,7 @@ import requests
 class TypeformDataAPI(object):
     def get_form_data(self, typeform_uid):
         typeform_api_key = settings.TYPEFORM_API_KEY
-
         url = f'https://api.typeform.com/v1/form/' \
             f'{typeform_uid}?key={typeform_api_key}'
-        requests.get(url)
+        response = requests.get(url)
+        return response.json()
--- intage_dash/typeforms/tests/test_api.py
+++ intage_dash/typeforms/tests/test_api.py
@@ -6,14 +6,22 @@ from ..api import TypeformDataAPI
 
 
 class TypeformDataAPITest(TestCase):
+    def setUp(self):
+        self.typeform_uid = 'HQxDoM'
+        self.typeform_api_key = 'api_key_test'
+        self.typeform_data_api = TypeformDataAPI()
+
     @patch('typeforms.api.requests')
     def test_get_form_data_should_call_correct_endpoint(self, mock):
-        typeform_uid = 'HQxDoM'
-        typeform_api_key = 'api_key_test'
-
-        typeform_data_api = TypeformDataAPI()
-        typeform_data_api.get_form_data(typeform_uid)
+        self.typeform_data_api.get_form_data(self.typeform_uid)
 
         expected_url = f'https://api.typeform.com/v1/form/' \
-            f'{typeform_uid}?key={typeform_api_key}'
+            f'{self.typeform_uid}?key={self.typeform_api_key}'
         mock.get.assert_called_once_with(expected_url)
+
+    @patch('typeforms.api.requests')
+    def test_get_form_data_should_return_response_in_json(self, mock):
+        expected = {'http_status': 200}
+        mock.get.return_value.json.return_value = expected
+        results = self.typeform_data_api.get_form_data(self.typeform_uid)
+        self.assertDictEqual(results, expected)
