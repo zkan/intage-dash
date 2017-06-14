a96611a8d7d148832f440846463a179464d43b10
zkan <kan@prontomarketing.com>
2017-06-14 20:21:02 +0700

    Register form response admin and show columns #2

---
+++ intage_dash/form_submissions/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import FormResponse
+
+
+@admin.register(FormResponse)
+class FormResponseAdmin(admin.ModelAdmin):
+    list_display = (
+        'typeform',
+        'answers',
+    )
---
+++ intage_dash/form_submissions/tests/test_admin.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from ..models import FormResponse
+from typeforms.models import Typeform
+
+
+class FormResponseAdminTest(TestCase):
+    def setUp(self):
+        User.objects.create_superuser('admin', 'admin@pronto.com', 'admin')
+        self.client.login(username='admin', password='admin')
+        self.url = '/admin/form_submissions/formresponse/'
+
+    def test_form_response_admin_should_be_accessible(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_response_admin_should_have_columns(self):
+        typeform = Typeform.objects.create(
+            uid='abc',
+            payload=[]
+        )
+        FormResponse.objects.create(
+            typeform=typeform,
+            answers=[]
+        )
+        response = self.client.get(self.url)
+
+        expected = '<div class="text"><a href="?o=1">Typeform</a></div>'
+        self.assertContains(response, expected, count=1, status_code=200)
+
+        expected = '<div class="text"><a href="?o=2">Answers</a></div>'
+        self.assertContains(response, expected, count=1, status_code=200)
