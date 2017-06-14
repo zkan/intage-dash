a4a66aafb1edf9cee6b61046a82701a4494838ef
zkan <kan@prontomarketing.com>
2017-06-14 14:51:33 +0700

    Override TYPEFORM_API_KEY settings for test

--- intage_dash/typeforms/tests/test_api.py
+++ intage_dash/typeforms/tests/test_api.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from django.test import TestCase
+from django.test import override_settings, TestCase
 
 from ..api import TypeformDataAPI
 
@@ -8,15 +8,16 @@ from ..api import TypeformDataAPI
 class TypeformDataAPITest(TestCase):
     def setUp(self):
         self.typeform_uid = 'HQxDoM'
-        self.typeform_api_key = 'api_key_test'
         self.typeform_data_api = TypeformDataAPI()
 
     @patch('typeforms.api.requests')
+    @override_settings(TYPEFORM_API_KEY='api_key_test')
     def test_get_form_data_should_call_correct_endpoint(self, mock):
+        typeform_api_key = 'api_key_test'
         self.typeform_data_api.get_form_data(self.typeform_uid)
 
         expected_url = f'https://api.typeform.com/v1/form/' \
-            f'{self.typeform_uid}?key={self.typeform_api_key}'
+            f'{self.typeform_uid}?key={typeform_api_key}'
         mock.get.assert_called_once_with(expected_url)
 
     @patch('typeforms.api.requests')
