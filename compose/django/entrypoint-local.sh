7ccf7ab5c733d3d3f6bfc8b60df7bcf003991e49
zkan <kan@prontomarketing.com>
2017-06-14 15:56:06 +0700

    Add sleep time #2

--- compose/django/entrypoint-local.sh
+++ compose/django/entrypoint-local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while ! nc -z db 5432; do sleep 1; done
+sleep 5
 cd $APPLICATION_ROOT\intage_dash
 python manage.py migrate --settings=intage_dash.settings
 python manage.py runserver 0.0.0.0:8000 --settings=intage_dash.settings
