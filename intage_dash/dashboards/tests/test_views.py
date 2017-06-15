bade27b79d87e7ea3fea2c5accfdf6c1264d3545
zkan <kan@prontomarketing.com>
2017-06-16 06:36:28 +0700

    Use Google Charts API #3

--- intage_dash/dashboards/tests/test_views.py
+++ intage_dash/dashboards/tests/test_views.py
@@ -3,7 +3,15 @@ from django.test import TestCase
 
 
 class DashboardViewTest(TestCase):
+    def setUp(self):
+        self.url = reverse('dashboard')
+
     def test_dashboard_view_should_be_accessible(self):
-        url = reverse('dashboard')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+
+    def test_dashboard_view_should_include_google_chart_api_script(self):
+        response = self.client.get(self.url)
+        expected = '<script type="text/javascript" ' \
+            'src="https://www.gstatic.com/charts/loader.js"></script>'
+        self.assertContains(response, expected, status_code=200)
