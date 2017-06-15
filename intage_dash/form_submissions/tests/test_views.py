383fceb31a99776889518cbbe7c9de9eddc63942
zkan <kan@prontomarketing.com>
2017-06-15 07:33:46 +0700

    Receive payload via webhook and save response #2

--- intage_dash/form_submissions/tests/test_views.py
+++ intage_dash/form_submissions/tests/test_views.py
@@ -1,13 +1,72 @@
+import json
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from ..models import FormResponse
+from typeforms.models import Typeform
+
 
 class ResponseWebhookViewTest(TestCase):
+    def setUp(self):
+        self.typeform = Typeform.objects.create(
+            uid='HQnxZM',
+            payload={}
+        )
+        self.data = {
+            'form_response': {
+                'form_id': 'HQnxZM',
+                'token': '2d5a18110c274a2f93958599918df1a7',
+                'answers': [
+                    {
+                        'field': {
+                            'type': 'multiple_choice',
+                            'id': '53368385'
+                        },
+                        'type': 'choice',
+                        'choice': {
+                            'label': 'BKK'
+                        }
+                    },
+                    {
+                        'field': {
+                            'type': 'rating',
+                            'id': '53368586'
+                        },
+                        'type': 'number',
+                        'number': 7
+                    }
+                ]
+            }
+        }
+        self.url = reverse('submission_webhook')
+
     def test_webhook_view_should_accept_post_request(self):
-        url = reverse('submission_webhook')
         response = self.client.post(
-            url,
-            data={'data': 'test'},
+            self.url,
+            data=json.dumps(self.data),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_webhook_view_should_create_response_when_receive_post_request(
+        self
+    ):
+        self.client.post(
+            self.url,
+            data=json.dumps(self.data),
+            content_type='application/json'
+        )
+
+        form_responses = FormResponse.objects.filter(typeform=self.typeform)
+
+        self.assertEqual(len(form_responses), 1)
+
+        expected_answers = {
+            'list_53368385_choice': 'BKK',
+            'rating_53368586': 7
+        }
+        self.assertDictEqual(form_responses[0].answers, expected_answers)
+
+        expected_token = '2d5a18110c274a2f93958599918df1a7'
+        self.assertEqual(form_responses[0].token, expected_token)
--- intage_dash/form_submissions/views.py
+++ intage_dash/form_submissions/views.py
@@ -1,7 +1,12 @@
+import json
+
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+
+from .models import FormResponse
+from typeforms.models import Typeform
 
 
 class ResponseWebhookView(View):
@@ -9,5 +14,31 @@ class ResponseWebhookView(View):
     def dispatch(self, *args, **kwargs):
         return super(ResponseWebhookView, self).dispatch(*args, **kwargs)
 
+    def _get_answers(self, payload):
+        answers = {}
+        for each in payload['form_response']['answers']:
+            if each['field']['type'] == 'multiple_choice':
+                key = 'list_' + each['field']['id'] + '_choice'
+                value = each['choice']['label']
+            elif each['field']['type'] == 'rating':
+                key = 'rating_' + each['field']['id']
+                value = each['number']
+            answers[key] = value
+
+        return answers
+
     def post(self, request):
+        payload = json.loads(request.body)
+
+        typeform = Typeform.objects.get(
+            uid=payload['form_response']['form_id']
+        )
+        answers = self._get_answers(payload)
+
+        FormResponse.objects.create(
+            typeform=typeform,
+            answers=answers,
+            token=payload['form_response']['token']
+        )
+
         return HttpResponse()
