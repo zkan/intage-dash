66c28b7d5a2c8cdafd916bc814514aaef0c1c316
zkan <kan@prontomarketing.com>
2017-06-18 10:13:20 +0700

    Render charts separated by branch #3

--- intage_dash/dashboards/tests/test_views.py
+++ intage_dash/dashboards/tests/test_views.py
@@ -10,9 +10,9 @@ class DashboardViewTest(TestCase):
         typeform_uid = 'iSEzWG'
         token = 'e65c13cb2a33255ff8d49994c8971e44'
         answers = {
-            'rating_53456466': '3',
-            'rating_53701739': '4',
-            'rating_53748628': '9',
+            'rating_53456466': 3,
+            'rating_53701739': 4,
+            'rating_53748628': 9,
         }
         payload = {
             'questions': [
@@ -93,7 +93,7 @@ class DashboardViewTest(TestCase):
         expected = '[\'Give me some rate.\', 3.0],'
         self.assertContains(response, expected, status_code=200)
 
-        expected = 'title: "This is just a question group..."'
+        expected = 'title: "This is just a question group...",'
         self.assertContains(response, expected, status_code=200)
 
         expected = 'var chart = new google.visualization.ColumnChart' \
@@ -112,7 +112,7 @@ class DashboardViewTest(TestCase):
         expected = '[\'Test another rating\', 4.0],'
         self.assertContains(response, expected, status_code=200)
 
-        expected = 'title: "Another question group..."'
+        expected = 'title: "Another question group...",'
         self.assertContains(response, expected, status_code=200)
 
         expected = 'var chart = new google.visualization.ColumnChart' \
@@ -145,6 +145,76 @@ class DashboardViewTest(TestCase):
 class DashboardBranchViewTest(TestCase):
     def setUp(self):
         typeform_uid = 'iSEzWG'
+        token = 'e65c13cb2a33255ff8d49994c8971e44'
+        another_token = 'a81c13cb2a33255ff8d49994c8971e55'
+        answers = {
+            'list_53368385_choice': 'BKK',
+            'rating_53456466': 3,
+            'rating_53701739': 4,
+            'rating_53748628': 9,
+        }
+        another_answers = {
+            'list_53368385_choice': 'Phuket',
+            'rating_53456466': 5,
+            'rating_53701739': 6,
+            'rating_53748628': 3,
+        }
+        payload = {
+            'questions': [
+                {
+                    'field_id': 53368385,
+                    'id': 'list_53368385_choice',
+                    'question': 'Branch?'
+                },
+                {
+                    'id': 'group_53700090',
+                    'field_id': 53700090,
+                    'question': 'This is just a question group'
+                },
+                {
+                    'id': 'rating_53456466',
+                    'group': 'group_53700090',
+                    'field_id': 53456466,
+                    'question': 'Give me some rate.'
+                },
+                {
+                    'id': 'group_53701933',
+                    'field_id': 53701933,
+                    'question': 'Another question group'
+                },
+                {
+                    'id': 'rating_53701739',
+                    'group': 'group_53701933',
+                    'field_id': 53701739,
+                    'question': 'Test another rating'
+                },
+                {
+                    'id': 'rating_53748628',
+                    'field_id': 53748628,
+                    'question': 'Extra question'
+                },
+            ],
+            'responses': [
+                {
+                    'token': token,
+                    'answers': answers,
+                },
+            ]
+        }
+        typeform = Typeform.objects.create(
+            uid=typeform_uid,
+            payload=payload
+        )
+        FormResponse.objects.create(
+            typeform=typeform,
+            answers=answers,
+            token=token
+        )
+        FormResponse.objects.create(
+            typeform=typeform,
+            answers=another_answers,
+            token=another_token
+        )
         self.url = reverse(
             'dashboard_branch',
             kwargs={
@@ -162,4 +232,71 @@ class DashboardBranchViewTest(TestCase):
         response = self.client.get(self.url)
         expected = '<script type="text/javascript" ' \
             'src="https://www.gstatic.com/charts/loader.js"></script>'
+        self.assertContains(response, expected, status_code=200)
+
+    def test_dashboard_branch_view_should_render_charts_for_each_branch(self):
+        response = self.client.get(self.url)
+
+        expected = 'google.charts.load("current", ' \
+            '{"packages": ["corechart"]});'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '[\'Score\', \'Phuket\', \'BKK\']'
+        self.assertContains(response, expected, count=3, status_code=200)
+
+        expected = 'google.charts.setOnLoadCallback(group_53700090);'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'function group_53700090()'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '[\'Give me some rate.\', 5.0, 3.0],'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'title: "This is just a question group...",'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'var chart = new google.visualization.ColumnChart' \
+            '(document.getElementById(\'group_53700090\'));'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<div id="group_53700090"></div>'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'google.charts.setOnLoadCallback(group_53701933);'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'function group_53701933()'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '[\'Test another rating\', 6.0, 4.0],'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'title: "Another question group...",'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'var chart = new google.visualization.ColumnChart' \
+            '(document.getElementById(\'group_53701933\'));'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<div id="group_53701933"></div>'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'google.charts.setOnLoadCallback(group_others);'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'function group_others()'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '[\'Extra question\', 3.0, 9.0]'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'title: "Others",'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = 'var chart = new google.visualization.ColumnChart' \
+            '(document.getElementById(\'group_others\'));'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<div id="group_others"></div>'
         self.assertContains(response, expected, status_code=200)
--- intage_dash/dashboards/views.py
+++ intage_dash/dashboards/views.py
@@ -61,7 +61,7 @@ class DashboardView(TemplateView):
         for idx, question in question_list:
             chart['data'].append(
                 [
-                    question,
+                    clean_html(question.replace('\xa0', '')),
                     df_answers[idx].mean()
                 ]
             )
@@ -80,7 +80,81 @@ class DashboardBranchView(TemplateView):
     template_name = 'dashboard_branch.html'
 
     def get(self, request, typeform_uid):
+        typeform = Typeform.objects.get(uid=typeform_uid)
+        questions = typeform.payload['questions']
+        df_questions = pd.DataFrame(questions)
+
+        form_responses = FormResponse.objects.filter(typeform=typeform)
+        answers = [each.answers for each in form_responses if each.answers]
+        df_answers = pd.DataFrame(answers)
+
+        branch_column_name = 'list_53368385_choice'
+        branches = list(filter(
+            lambda x: type(x) == str, df_answers[branch_column_name].unique()
+        ))
+
+        question_groups = list(filter(
+            lambda x: type(x) == str, df_questions.group.unique()
+        ))
+
+        charts = []
+        for each_question_group in question_groups:
+            chart = {'data': []}
+            chart['id'] = each_question_group
+            chart['label'] = df_questions[
+                df_questions['id'] == each_question_group
+            ]['question'].iloc[0] + '...'
+            questions_in_group = df_questions[
+                df_questions['group'] == each_question_group
+            ][['id', 'question']]
+            question_list = zip(
+                questions_in_group.id,
+                questions_in_group.question
+            )
+            for question_id, question in question_list:
+                for idx, each_branch in enumerate(branches):
+                    if idx == 0:
+                        data_point = [
+                            clean_html(question.replace('\xa0', ''))
+                        ]
+                    df_branch = df_answers[
+                        df_answers[branch_column_name] == each_branch
+                    ]
+                    data_point.append(df_branch[question_id].mean())
+                chart['data'].append(data_point)
+            charts.append(chart)
+
+        chart = {'data': []}
+        chart['id'] = 'group_others'
+        chart['label'] = 'Others'
+        questions_in_no_group = df_questions[
+            df_questions['group'].isnull() &
+            df_questions['id'].str.contains('rating_')
+        ][['id', 'question']]
+        question_list = zip(
+            questions_in_no_group.id,
+            questions_in_no_group.question
+        )
+        for question_id, question in question_list:
+            for idx, each_branch in enumerate(branches):
+                if idx == 0:
+                    data_point = [
+                        clean_html(question.replace('\xa0', ''))
+                    ]
+                df_branch = df_answers[
+                    df_answers[branch_column_name] == each_branch
+                ]
+                data_point.append(df_branch[question_id].mean())
+            chart['data'].append(data_point)
+        charts.append(chart)
+
+        branches = ['Score'] + branches
+
         return render(
             request,
-            self.template_name
+            self.template_name,
+            {
+                'branches': branches,
+                'charts': charts
+            }
         )
